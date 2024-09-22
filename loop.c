653fd36950c516027eba8bf50bb812c929a63379
SHK KAFEEL <137709969+shkkafeel@users.noreply.github.com>
2024-09-22 21:17:34 +0530

    Add files via upload

---
+++ loop.c
@@ -0,0 +1,44 @@
+// #include <stdio.h>
+
+// FOR LOOP
+
+// int main()
+// {
+//     int arr[] = {99, 88, 77, 66, 55};
+
+//     for (int i = 0; i <= 5; i++)
+//     {
+//         printf("%d", arr[i]);    // 99, 88, 77, 66, 55
+//     }
+
+//     return 0;
+// }
+
+// WHILE LOOP
+// #include <stdio.h>
+
+// int main()
+// {
+
+//     int i = 3;
+//     while (i <= 30)
+//     {
+//         printf("%d \n", i);
+
+//         i = i + 3;
+//     }
+//     return 0;
+// }
+
+// TABLE OF
+
+// #include <stdio.h>
+// int main()
+// {
+//     for (int i = 6; i <= 60; i = i + 6)
+//     {
+
+//         printf("%d \n", i);
+//     }
+//     return 0;
+// }
